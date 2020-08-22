66c7312c493e78f9b8eddeca15e5919bb6187dfc
Senbonzakura <anhdungpham090@gmail.com>
2020-08-22 16:56:46 +0700

    update seeder.

--- src/main/java/com/app/manager/controller/HomeController.java
+++ src/main/java/com/app/manager/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.app.manager.controller;
 
+import com.app.manager.model.Seeder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+    @Autowired Seeder seeder;
+
     @RequestMapping({"/home", "/"})
     public String index() {
+//        seeder.Seed(); // uncomment this to seed data, comment it again when done seeding
         return "views/home";
     }
 }
