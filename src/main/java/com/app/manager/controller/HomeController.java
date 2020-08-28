4aa91c952ecaa273760dbaf0d42b29a75451db2b
Senbonzakura <anhdungpham090@gmail.com>
2020-08-28 21:04:31 +0700

    comment seeder.

--- src/main/java/com/app/manager/controller/HomeController.java
+++ src/main/java/com/app/manager/controller/HomeController.java
@@ -3,7 +3,6 @@ package com.app.manager.controller;
 import com.app.manager.model.Seeder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,7 +12,7 @@ public class HomeController {
 
     @RequestMapping({"/home", "/"})
     public String index() {
-        seeder.Seed(); // uncomment this to seed data, comment it again when done seeding
+//        seeder.Seed(); // uncomment this to seed data, comment it again when done seeding
         return "views/home";
     }
 }
