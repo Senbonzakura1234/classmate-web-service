dae64140c598f97c0e1649a1ba6d5ef1d93ff872
Senbonzakura <anhdungpham090@gmail.com>
2020-09-09 19:59:19 +0700

    fix homepage bug.

--- src/main/java/com/app/manager/controller/HomeController.java
+++ src/main/java/com/app/manager/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.app.manager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping({"/", "/home"})
     public String index() {
         return "views/home";
     }
